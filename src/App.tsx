4071a2b0578c583efcd9fb234a88bc8fd5ded9ba
ishigami-prart <ishigami.takatoshi@prart.jp>
2022-07-04 07:56:51 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { UserCard } from "./components/UserCard";
+import "./styles.css";
+
+const user = {
+  id: 1,
+  name: "じゃけえ",
+  email: "aaa",
+  addredd: "bbbb"
+};
+
+export default function App() {
+  return (
+    <div className="App">
+      <button></button>
+    </div>
+  );
+}
