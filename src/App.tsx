bf25616fc63bef465239a75e658844db6c072076
ishigami-prart <ishigami.takatoshi@prart.jp>
2022-07-04 08:12:19 +0900

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { UserCard } from "./components/UserCard";
 import "./styles.css";
+import { User } from "./types/api/user";
 
 const user = {
   id: 1,
@@ -10,12 +11,18 @@ const user = {
 };
 
 export default function App() {
+  const [userProfile, setuserProfiles] = userState<Array<userProfile>>([]);
   const onClickFetchUser = () => {
-    axios.get("https://jsonplaceholder.typicode.com/users");
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map(user);
+      });
   };
   return (
     <div className="App">
       <button onClick={onClickFetchUser}>データ取得</button>
+      <UserCard user={user} />
     </div>
   );
 }
